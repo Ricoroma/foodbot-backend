d73d8d393a0121f2611a5633e1cdb41eece74a9f
Ricoroma <romansafronov0609@gmail.com>
2024-04-05 21:09:19 +0300

    - микрофикс бд - перенес айди юзера из хедерсов в путь

--- src/controllers/profile_controller.py
+++ src/controllers/profile_controller.py
@@ -6,14 +6,13 @@ from sqlalchemy.orm import Session
 
 from .telegram_controller import bot
 from ..config.database.database import MenuOption, User, sqlalchemy_to_pydantic, Cart, PositionInCart, Order
-from ..support.dependencies import get_session, get_user_id
+from ..support.dependencies import get_session
 from ..support.schemas import MenuOptionModel, UpdateCartRequest, CartModel, ClaimWay, OrderModel, ProfileModel
 
 router = APIRouter(prefix="/profile", tags=['profile'])
 
 
-@router.get('/', response_model=ProfileModel)
-async def get_profile_by_id(db_session: Session = Depends(get_session),
-                            user_id: int = Depends(get_user_id)):
+@router.get('/{user_id}', response_model=ProfileModel)
+async def get_profile_by_id(user_id: int, db_session: Session = Depends(get_session)):
     user = db_session.query(User).get(user_id)
     return ProfileModel(**user.__dict__, orders=[OrderModel(**i.__dict__) for i in user.orders])
